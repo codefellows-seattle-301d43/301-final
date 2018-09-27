cb8d9e25cf63260217062a4ef6992ea79bf9aee9
ellisken <ellisken@oregonstate.edu>
2018-09-27 16:08:25 -0700

    fix delete message

--- public/scripts/delete.js
+++ public/scripts/delete.js
@@ -1,3 +1,7 @@
 $('#delete').on('submit', () => {
   return confirm('Are you sure you want to remove this patient?');
 });
+
+$('#deleteRec').on('submit', () => {
+  return confirm('Are you sure you want to remove this record?');
+});
