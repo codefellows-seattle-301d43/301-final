35003b551454ebf785f0ff68a572e7c33b56db24
Earnest Cain <caintre03@gmail.com>
2018-09-28 12:22:12 -0700

    fixed the table going over the header bar

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -6,6 +6,7 @@ $(document).ready(() => {
     $('.social').toggleClass('show');
     $('.menu-toggle-one').hide();
     $('.menu-toggle-two').show();
+    $('#search-patient').hide();
   });
 
   $('.menu-toggle-two').click(() => {
@@ -13,6 +14,7 @@ $(document).ready(() => {
     $('.social').toggleClass('show');
     $('.menu-toggle-two').hide();
     $('.menu-toggle-one').show();
+    $('#search-patient').show();
   });
 
 });
